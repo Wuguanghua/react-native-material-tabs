efd002d3352989698edbee5f1e3760abd52111c4
武光华 <wuguanghua@ynsy.com>
2019-08-22 16:02:20 +0800

    change indicator style

--- src/components/Indicator.tsx
+++ src/components/Indicator.tsx
@@ -11,10 +11,17 @@ interface BarProps {
 
 const Bar = styled(Animated.View)`
   height: ${constants.indicatorHeight};
-  width: ${(props: BarProps) => props.tabWidth};
+  width: ${(props: BarProps) => props.tabWidth * 0.2};
   position: absolute;
   bottom: 0;
+  left: ${(props: BarProps) => props.tabWidth * 0.8 / 2};
   background-color: ${(props: BarProps) => props.color};
+  border-radius: 1;
+  elevation: 3;
+  shadow-color: ${(props: BarProps) => props.color};
+  shadow-opacity: 0.5;
+  shadow-radius: 2;
+  shadow-offset: { width: 0.5, height: 0.4 }
 `;
 
 interface IndicatorProps {
